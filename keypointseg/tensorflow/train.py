fdd6ad2e8861352632e35cf6d51a9403f9be74df
nenad <nenad.markus@fer.hr>
2017-09-21 22:01:37 +0200

    ...

--- keypointseg/tensorflow/train.py
+++ keypointseg/tensorflow/train.py
@@ -11,15 +11,14 @@ x = tf.placeholder(tf.float32, [None, maxhw, maxhw, 3])
 y = tf.placeholder(tf.float32, [None, maxhw, maxhw, 3])
 
 from models import *
-pred = make_convnet(x, 3) # `x` is input, `3` specifies the number of output channels
+pred = make_tinysegnet(x, 3) # `x` is input, `3` specifies the number of output channels
 
 # prepare the loss function
 thr = 0.1
 loss = tf.reduce_mean( tf.nn.relu( 0.5*(y - pred)**2 - 0.5*thr**2 ) )
 
 # we will use RMSprop to learn the model
-step = tf.train.RMSPropOptimizer(0.01).minimize(loss)
-<<<<<<< HEAD
+step = tf.train.RMSPropOptimizer(1e-4).minimize(loss)
 
 #
 saver = tf.train.Saver()
@@ -37,9 +36,9 @@ for k in range(2048):
 	l = sess.run(loss, feed_dict={x: X, y: Y, is_training: False})
 	print('* loss on batch %d: %.7f' % (k, l))
 	#
-	u = sess.run(pred, feed_dict={x: X, is_training: False})
-	import cv2
-	cv2.imwrite('wat.jpg', 255*u[0, :, :, :])
+	#u = sess.run(pred, feed_dict={x: X, is_training: False})
+	#import cv2
+	#cv2.imwrite('wat.jpg', 255*u[0, :, :, :])
 
 #
 import os
